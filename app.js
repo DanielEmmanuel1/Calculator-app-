3be9e4cf1ae0108762ec821764bac7f8694bec74
Crypt.sol.codes <110722148+DanielEmmanuel1@users.noreply.github.com>
2023-09-25 16:56:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const displayBoard = document.querySelector('.content-board');
+\const displayContainer = document.querySelector('.display-container');
+const displayContent = document.getElementById('display-content');
+const displayBoard = displayContainer.querySelector('.content-board');
 const allButtons = document.querySelectorAll('button');
+let input = '';
 
 allButtons.forEach(button => {
     button.addEventListener('click', () => {
-        if (button.classList.contains('AC') || button.classList.contains('equal')) {
-            // Do nothing for AC and equal buttons
-            return;
-        }
         const buttonText = button.textContent;
         const textNode = document.createTextNode(buttonText);
         const span = document.createElement('span');
@@ -14,12 +13,36 @@ allButtons.forEach(button => {
         span.style.color = 'white';
         span.style.fontSize = '25px';
         span.style.paddingRight = '5px';
-        displayBoard.appendChild(span);
+        displayContent.appendChild(span);
 
-        // ac button
+        if (buttonText === 'AC') {
+            input = '';
+            displayContent.textContent = '';
+
+        } else if (buttonText === '←') {
+            input = input.slice(0, -1);
+            displayContent.textContent = input;
+            
+        } else if (buttonText === '=') {
+            try {
+                input = eval(input);
+                const resultSpan = document.createElement('span');
+                resultSpan.textContent = input;
+                resultSpan.style.color = 'white';
+                resultSpan.style.fontSize = '25px';
+                resultSpan.style.paddingRight = '5px';
+                displayContent.appendChild(resultSpan);
+
+            } catch (error) {
+                displayContent.textContent = 'Error';
+            }
+        } else {
+            input += buttonText;
+        }
+
         const refreshButton = document.querySelector('.AC')
         refreshButton.addEventListener('click', () => {
-            displayBoard.textContent = '';
+            displayContent.textContent = '';
         });
     });
 });
